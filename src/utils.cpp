e5ed94f1c0d818e35d81f291b1e256423ac756e9
John <jacastro@dtic.ua.es>
2021-04-04 02:47:58 +0200

    Several functions refactored to utils

--- src/utils.cpp
+++ src/utils.cpp
@@ -9,6 +9,40 @@ bool utils::lineParser(const std::string& line, std::string& value, int& i, char
     return i < line.size();
 }
 
+bool utils::lineParser(const std::wstring& line, std::wstring& value, int& i, char delimiter){
+    while(line[i] != delimiter && i<line.size()){
+        value += line[i];
+        ++i;
+    }
+    ++i;
+    return i < line.size();
+}
+
 int utils::charExcelToNumber(const std::string& col){
     return col[0]-'a';
+}
+
+void utils::tolower(std::string& s){
+    for(int i=0; i<s.length(); ++i){
+        if(s[i]>='A' && s[i]<='Z')
+            s[i] -= ('Z'-'z');
+    }
+}
+
+void utils::splitCell(const std::string& cell, std::string& row, std::string& col){
+    int i=0;
+    while(i<cell.size() && cell[i] >= 'a' && cell[i] <= 'z'){
+        col.append(1,cell[i]);
+        ++i;
+    }
+    while(i<cell.size()){
+        row.append(1,cell[i]);
+        ++i;
+    }
+}
+
+void utils::excelToIndex(const std::string& cell, int& row_i, int& col_i){
+    std::string row_s, col_s;
+    splitCell(cell, row_s, col_s);
+    row_i = atoi(row_s.c_str())-1; col_i = charExcelToNumber(col_s);
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -7,8 +7,12 @@
 namespace utils{
 	//Return false if endline
 	bool lineParser(const std::string& line, std::string& value, int& i, char delimiter=';');
+	bool lineParser(const std::wstring& line, std::wstring& value, int& i, char delimiter=';');
 	//Limited to a-z
 	int charExcelToNumber(const std::string& col);
+	void tolower(std::string& s);
+	void excelToIndex(const std::string& cell, int& row_i, int& col_i);
+	void splitCell(const std::string& cell, std::string& row, std::string& col);
 }
 
 #endif
